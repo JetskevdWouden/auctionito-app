173f414a256d4d54629328b520cbc9a8a6f41681
JetskevdWouden <zegmaarjet@gmail.com>
2019-07-03 11:56:32 +0200

    users model

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const db = require('./db');
+//bodyparser
+
+//MODELS
+const User = require('./user/model');
+
+//ROUTERS
 
 const app = express();               //this is my server
+
+
+//jsonParser
+
+//APP.USE ROUTERS
+
+
+
+
 
 const port = process.env.PORT || 4000;
 
